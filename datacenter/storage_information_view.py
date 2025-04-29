9a79eb88b621b7beb6400d6524f1a477b1caddb1
darya-k00 <dasha.kon.060203@gmail.com>
2025-04-29 15:28:50 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,22 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-
+from datacenter.models import format_duration, get_duration, is_visit_long
+from django.utils.timezone import localtime
 
 def storage_information_view(request):
     # Программируем здесь
+    open_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in open_visits:
+        duration = get_duration(visit)
+        non_closed_visits.append({
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': localtime(visit.entered_at),
+            'duration': format_duration(duration),
+            'is_strange': is_visit_long(visit, minutes=60)
+        })
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
