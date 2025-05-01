45534188822414d42292b0de9a916a2189c04259
darya-k00 <dasha.kon.060203@gmail.com>
2025-05-01 18:01:09 +0300

    спрятала данные

--- project/settings.py
+++ project/settings.py
@@ -19,8 +19,7 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = env.str('DB_SECRET_KEY')
 
-DEBUG = True
-
+DEBUG =  env.bool('DEBUG', default=False)
 ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = ['*']
